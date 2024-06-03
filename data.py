781e152cd58af0208800d080e8da2442ee129059
Susanna Kayed <kefka.sk@gmail.com>
2024-06-03 12:08:03 +0200

    added api call to open trivia db

--- data.py
+++ data.py
@@ -1,73 +1,18 @@
-question_data = [{"category": "History", "type": "boolean", "difficulty": "easy",
-                  "question": "The Cold War ended with Joseph Stalin's death.",
-                  "correct_answer": "False", "incorrect_answers": ["True"]},
-                 {"category": "Entertainment: Music", "type": "boolean",
-                  "difficulty": "medium",
-                  "question": "Soulja Boy's Crank That; won a Grammy for Best Rap Song in 2007.",
-                  "correct_answer": "False", "incorrect_answers": ["True"]},
-                 {"category": "Entertainment: Film", "type": "boolean",
-                  "difficulty": "easy",
-                  "question": "The film '2001: A Space Odyssey'; was released on December 31st, 2000.",
-                  "correct_answer": "False", "incorrect_answers": ["True"]},
-                 {"category": "General Knowledge", "type": "boolean",
-                  "difficulty": "medium",
-                  "question": "Coca-Cola's original colour was green.",
-                  "correct_answer": "False", "incorrect_answers": ["True"]},
-                 {"category": "History", "type": "boolean", "difficulty": "medium",
-                  "question": "The M41 Walker Bulldog remains in service in some countries to this day.",
-                  "correct_answer": "True", "incorrect_answers": ["False"]},
-                 {"category": "Entertainment: Video Games", "type": "boolean",
-                  "difficulty": "easy",
-                  "question": "In 'Super Mario 64', collecting 100 coins on a level will give you a 1-UP.",
-                  "correct_answer": "False", "incorrect_answers": ["True"]},
-                 {"category": "Art", "type": "boolean", "difficulty": "medium",
-                  "question": "Vincent van Gogh cut off one of his ears.",
-                  "correct_answer": "True", "incorrect_answers": ["False"]},
-                 {"category": "Entertainment: Film", "type": "boolean",
-                  "difficulty": "medium",
-                  "question": "The color of the pills in the Matrix were Blue and Yellow.",
-                  "correct_answer": "False", "incorrect_answers": ["True"]},
-                 {"category": "General Knowledge", "type": "boolean",
-                  "difficulty": "medium",
-                  "question": "The bikini is named after the &quot;Bikini Atoll&quot;, an island where the United States conducted tests on atomic bombs.",
-                  "correct_answer": "True", "incorrect_answers": ["False"]},
-                 {"category": "Mythology", "type": "boolean", "difficulty": "medium",
-                  "question": "According to Norse mythology, Loki is a mother.",
-                  "correct_answer": "True", "incorrect_answers": ["False"]},
-                 {"category": "History", "type": "boolean", "difficulty": "medium",
-                  "question": "Theodore Roosevelt Jr. was the only General involved in the initial assault on D-Day.",
-                  "correct_answer": "True", "incorrect_answers": ["False"]},
-                 {"category": "Entertainment: Japanese Anime & Manga",
-                  "type": "boolean", "difficulty": "easy",
-                  "question": "No Game No Life first aired in 2014.",
-                  "correct_answer": "True", "incorrect_answers": ["False"]},
-                 {"category": "History", "type": "boolean", "difficulty": "easy",
-                  "question": "The United States of America declared their independence from the British Empire on July 4th, 1776.",
-                  "correct_answer": "True", "incorrect_answers": ["False"]},
-                 {"category": "Entertainment: Books", "type": "boolean",
-                  "difficulty": "easy",
-                  "question": "&quot;Elementary, my dear Watson&quot; is a phrase that is never truly said within the Conan Doyle books of Sherlock Holmes.",
-                  "correct_answer": "True", "incorrect_answers": ["False"]},
-                 {"category": "Entertainment: Music", "type": "boolean",
-                  "difficulty": "easy",
-                  "question": "The song &quot;Stronger Than You&quot; is a single by Estelle, who played Garnet in Steven Universe.",
-                  "correct_answer": "True", "incorrect_answers": ["False"]},
-                 {"category": "Science: Mathematics", "type": "boolean",
-                  "difficulty": "medium", "question": "E = MC3",
-                  "correct_answer": "False", "incorrect_answers": ["True"]},
-                 {"category": "Entertainment: Video Games", "type": "boolean",
-                  "difficulty": "easy",
-                  "question": "In &quot;Undertale&quot;, the main character of the game is Sans.",
-                  "correct_answer": "False", "incorrect_answers": ["True"]},
-                 {"category": "Entertainment: Television", "type": "boolean",
-                  "difficulty": "medium",
-                  "question": "In &quot;Star Trek: The Next Generation&quot;, Data is the only android in existence.",
-                  "correct_answer": "False", "incorrect_answers": ["True"]},
-                 {"category": "Entertainment: Music", "type": "boolean",
-                  "difficulty": "medium",
-                  "question": "&quot;Neutral Milk Hotel&quot; is a real band.",
-                  "correct_answer": "True", "incorrect_answers": ["False"]},
-                 {"category": "Entertainment: Film", "type": "boolean",
-                  "difficulty": "medium",
-                  "question": "The original ending of &quot;Little Shop Of Horrors&quot; has the plants taking over the world.",
-                  "correct_answer": "True", "incorrect_answers": ["False"]}]
+import requests
+
+ENDPOINT = "https://opentdb.com/api.php?"
+
+parameters ={
+    "amount":10,
+    "type":"boolean"
+}
+
+response = requests.get(ENDPOINT,params=parameters)
+response.raise_for_status()
+
+data = response.json()
+question_data = data['results']
+
+
+
+
