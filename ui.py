52f87144d1f4b25e99b48cccebbd09f3b7c4b436
Susanna Kayed <kefka.sk@gmail.com>
2024-06-04 16:10:40 +0200

    added user feedback and right/wrong functions for UI buttons

--- ui.py
+++ ui.py
@@ -7,11 +7,10 @@ class QuizUi():
 
     def __init__(self, quiz_questions: QuizBrain):
         self.quiz = quiz_questions
+
         self.window = Tk()
         self.window.title("Quizzler")
         self.window.config(background=THEME_COLOR, padx=20, pady=20)
-
-        
 
         self.canvas = Canvas(width=300, height=250, bg="white")
         self.question_text = self.canvas.create_text(
@@ -27,11 +26,11 @@ class QuizUi():
         self.score_label.grid(column=1, row=0)
 
         true_img = PhotoImage(file="images/true.png")
-        self.true = Button(borderwidth=0, image=true_img, highlightthickness=0)
+        self.true = Button(borderwidth=0, image=true_img, highlightthickness=0, command= self.true_ans)
         self.true.grid(column=0,row=2)
 
         false_img = PhotoImage(file="images/false.png")
-        self.false = Button(borderwidth=0, image=false_img, highlightthickness=0)
+        self.false = Button(borderwidth=0, image=false_img, highlightthickness=0, command= self.false_ans)
         self.false.grid(column=1,row=2)
         
         self.next_question()
@@ -41,6 +40,25 @@ class QuizUi():
         self.window.mainloop()
     
     def next_question(self):
+        self.canvas.config(bg="white")
         q_text= self.quiz.next_question()
         self.canvas.itemconfig(self.question_text, text=q_text)
+        
+
+    def true_ans(self):
+        self.give_feedback(self.quiz.check_answer("true"))
+    
+    def false_ans(self):
+        self.give_feedback(self.quiz.check_answer("false"))
+    
+    def give_feedback(self, is_right):
+        if is_right:
+            self.canvas.config(bg="green")
+        else:
+            self.canvas.config(bg="red")
+            
+        self.window.after(1000, self.next_question)
+
+    
+        
 
