6e2caf2910dd3fdd39795167fa11a26df28e9152
Susanna Kayed <kefka.sk@gmail.com>
2024-06-04 11:18:56 +0200

    added dynamic text to the UI

--- ui.py
+++ ui.py
@@ -1,10 +1,12 @@
 from tkinter import *
+from quiz_brain import QuizBrain
+
 THEME_COLOR = "#375362"
 
 class QuizUi():
 
-    def __init__(self):
-
+    def __init__(self, quiz_questions: QuizBrain):
+        self.quiz = quiz_questions
         self.window = Tk()
         self.window.title("Quizzler")
         self.window.config(background=THEME_COLOR, padx=20, pady=20)
@@ -12,7 +14,12 @@ class QuizUi():
         
 
         self.canvas = Canvas(width=300, height=250, bg="white")
-        self.question_text = self.canvas.create_text(150,125, text="testo domanda", font=("Arial", 20, "italic"))
+        self.question_text = self.canvas.create_text(
+            150,
+            125, 
+            text="testo domanda", 
+            font=("Arial", 20, "italic"),
+            width=280)
         self.canvas.grid(column=0,row=1, columnspan=2, pady=50)
 
         
@@ -27,9 +34,13 @@ class QuizUi():
         self.false = Button(borderwidth=0, image=false_img, highlightthickness=0)
         self.false.grid(column=1,row=2)
         
-        
+        self.next_question()
         
 
 
         self.window.mainloop()
-        
+    
+    def next_question(self):
+        q_text= self.quiz.next_question()
+        self.canvas.itemconfig(self.question_text, text=q_text)
+
